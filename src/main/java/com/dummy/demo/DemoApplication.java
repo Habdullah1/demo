01c07245ecf67603da13aa2e17a040f7c688f01a
Habdullah1 <dis_716@hotmail.com>
2022-02-28 06:24:02 -0600

    added new mock service

--- src/main/java/com/dummy/demo/DemoApplication.java
+++ src/main/java/com/dummy/demo/DemoApplication.java
@@ -8,11 +8,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 @SpringBootApplication
 public class DemoApplication {
 
+	private static Map<String, String> headersMap;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		headersMap = new HashMap<>();
+		headersMap.put("content-type", "application/json; charset=UTF-8");
+		headersMap.put("x-frame-options", "SAMEORIGIN");
+		headersMap.put("x-content-type-options", "nosniff");
+		headersMap.put("x-xss-protection", "1; mode=block");
+		headersMap.put("x-ratelimit-remaining", "299");
+		headersMap.put("x-ratelimit-used", "1");
+		headersMap.put("x-ratelimit-reset", "71");
+		headersMap.put("cache-control", "max-age=0, must-revalidate");
+		headersMap.put("x-moose", "majestic");
+		headersMap.put("accept-ranges", "bytes");
+		headersMap.put("date", "Sun, 27 Feb 2022 18:58:49 GMT");
+		headersMap.put("via", "1.1 varnish");
+		headersMap.put("set-cookie", "edgebucket=OXiitxTkDk5fDo5UpN; Domain=reddit.com; Max-Age=63071999; Path=/;  secure");
+		headersMap.put("strict-transport-security", "max-age=15552000; includeSubDomains; preload");
+		headersMap.put("server", "snooserv");
+		headersMap.put("content-length", "122");
+
 	}
 
 
@@ -22,27 +47,32 @@ public class DemoApplication {
 		public ResponseEntity<String> getStuff() {
 			String body = "{\"access_token\": \"1578141871049-Aepb0E2P3YWz3jPiN0AjidEfQti0Lw\", \"token_type\": \"bearer\", \"expires_in\": 3600, \"scope\": \"*\"}";
 			System.out.println("Got Request");
-			return ResponseEntity.ok().headers(c -> {
-				c.add("content-type", "application/json; charset=UTF-8");
-				c.add("x-frame-options", "SAMEORIGIN");
-				c.add("x-content-type-options", "nosniff");
-				c.add("x-xss-protection", "1; mode=block");
-				c.add("x-ratelimit-remaining", "299");
-				c.add("x-ratelimit-used", "1");
-				c.add("x-ratelimit-reset", "71");
-				c.add("cache-control", "max-age=0, must-revalidate");
-				c.add("x-moose", "majestic");
-				c.add("accept-ranges", "bytes");
-				c.add("date", "Sun, 27 Feb 2022 18:58:49 GMT");
-				c.add("via", "1.1 varnish");
-				c.add("set-cookie", "edgebucket=OXiitxTkDk5fDo5UpN; Domain=reddit.com; Max-Age=63071999; Path=/;  secure");
-				c.add("strict-transport-security", "max-age=15552000; includeSubDomains; preload");
-				c.add("server", "snooserv");
-				c.add("content-length", "122");
-
-			})
+			return ResponseEntity.ok()
+					.headers(c -> headersMap.forEach(c::add))
 					.body(body);
 		}
+
+		@GetMapping("/r/technology/new")
+		public ResponseEntity<String> getRedditPost() {
+			InputStream input = DemoApplication.class.getResourceAsStream("/demo.txt");
+			StringBuilder sb = new StringBuilder();
+			try {
+				byte[] bytes = Objects.requireNonNull(input).readAllBytes();
+				System.out.println("NUmber of bytes = " + bytes.length);
+				for (byte aByte : bytes) {
+					sb.append((char) aByte);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			ResponseEntity<String> body = ResponseEntity.ok()
+					.headers(c -> headersMap.forEach(c::add))
+					.body(sb.toString());
+			System.out.println(body);
+			return body;
+		}
 	}
+
 
 }
